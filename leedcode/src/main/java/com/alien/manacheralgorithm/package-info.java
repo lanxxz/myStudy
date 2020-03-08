874dbf413738e277705db7843df6c13656395d0b
alien <15080165456@163.com>
2020-03-08 21:05:55 +0800

    马拉车算法

---
+++ leedcode/src/main/java/com/alien/manacheralgorithm/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * program: myStudy
+ * description:
+ * 针对最长回文子串相关的问题，马拉车算法应该是比较通用的解法 <br/>
+ * 参考博客：https://mp.weixin.qq.com/s/WIKOdF3BkqnM_lnr9GxyUA
+ *
+ *
+ * @author: alien
+ * @since: 2020/03/08 19:57
+ */
+package com.alien.manacheralgorithm;
