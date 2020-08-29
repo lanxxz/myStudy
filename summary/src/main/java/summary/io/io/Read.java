7c16019549df84932864670b770ffe423514e9b9
alien <15080165456@163.com>
2020-08-29 21:25:26 +0800

    文件的读取和创建的几种方法总结

---
+++ summary/src/main/java/summary/io/io/Read.java
@@ -0,0 +1,188 @@
+package summary.io.io;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * program: myStudy
+ * description: Java读文件的方法总结
+ *
+ * @author: alien
+ * @since: 2020/08/29 20:40
+ */
+public class Read {
+
+    /**
+     * method name: testReadFile <br/>
+     * description: {@link java.util.Scanner} Java 5 引入
+
+     * @return: void
+     * @since: 2020/8/29
+     */
+    static void testReadFile() throws FileNotFoundException {
+        String fileName = "./newFile1.txt";
+
+        // 一行一行的读
+        try (Scanner sc = new Scanner(new FileReader(fileName))) {
+            while (sc.hasNextLine()) {
+                final String line = sc.nextLine();
+                System.out.println(line);
+            }
+        }
+        System.out.println("===========");
+
+        // 按指定分隔符读
+        try (Scanner sc = new Scanner(new FileReader(fileName))) {
+            sc.useDelimiter("\\|");
+            while (sc.hasNext()) {
+                final String next = sc.next();
+                System.out.println(next);
+            }
+        }
+        System.out.println("================");
+
+        // 按基础类型来读取 如: 整数、浮点数等
+        String fileName2 = "./newFile2.txt";
+        try (Scanner sc = new Scanner(new FileReader(fileName2))) {
+            sc.useDelimiter("\\|");
+            while (sc.hasNextInt()) {
+                int integer = sc.nextInt();
+                System.out.println(integer);
+            }
+        }
+
+    }
+
+    /**
+     * method name: testReadFile2 <br/>
+     * description: Java 8 引入
+
+     * @return: void
+     * @since: 2020/8/29
+     */
+    static void testReadFile2() throws IOException {
+        String fileName = "./newFile1.txt";
+
+        final Stream<String> lines = Files.lines(Paths.get(fileName));
+
+        // 随机顺序进行数据处理
+        lines.forEach(System.out::println);
+        System.out.println("===========");
+
+        final Stream<String> lines2 = Files.lines(Paths.get(fileName)); // 注意流只能被消费一次
+        // 按文件顺序进行数据处理
+        lines2.forEachOrdered(System.out::println);
+        System.out.println("=============");
+
+        // 以下要注意 java.lang.OutOfMemoryError: Java heap space 内存溢出
+        // 转换成 List<String>
+        final Stream<String> lines3 = Files.lines(Paths.get(fileName)); // 注意流只能被消费一次
+        final List<String> collect = lines3.collect(Collectors.toList());
+        final Stream<String> lines4 = Files.lines(Paths.get(fileName)); // 注意流只能被消费一次
+        final String content = lines4.collect(Collectors.joining());
+        System.out.println(content);
+
+    }
+
+    /**
+     * method name: testReadFlie3 <br/>
+     * description: Java 1.7 引入
+
+     * @return: void
+     * @since: 2020/8/29
+     */
+    static void testReadFlie3() throws IOException {
+        String fileName = "./newFile1.txt";
+
+        // 以下要注意 java.lang.OutOfMemoryError: Java heap space 内存溢出
+        final List<String> list = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+        list.forEach(System.out::println); // jdk 8 新增的方法
+    }
+
+    /**
+     * method name: testReadFile4 <br/>
+     * description: Java 11 引入
+
+     * @return: void
+     * @since: 2020/8/29
+     */
+    static void testReadFile4() throws IOException {
+        String fileName = "./newFile1.txt";
+
+        // 读取一篇文章可以使用这种方式
+        // Java 11 新增的方法
+        // 读取的文件不能超过 2G, 与你的内存息息相关
+//        final String s = Files.readString(Paths.get(fileName));
+//        System.out.println(s);
+    }
+
+    /**
+     * method name: testReadFile5 <br/>
+     * description: Java 1.7 引入
+
+     * @return: void
+     * @since: 2020/8/29
+     */
+    static void testReadFile5() throws IOException {
+        String fileName = "./newFile1.txt";
+
+        // 读取一篇文章可以使用这种方式
+        // Java 1.7 新增的方法
+        // 读取的文件不能超过 2G, 与你的内存息息相关
+        final byte[] bytes = Files.readAllBytes(Paths.get(fileName));
+
+        final String content = new String(bytes, StandardCharsets.UTF_8);
+        System.out.println(content);
+    }
+
+    static void testReadFIle6() throws IOException, ClassNotFoundException {
+        // 管道流的方式，比较灵活
+        String fileName = "./newFile1.txt";
+        try(BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+        System.out.println("========");
+
+        // Java 8 新的写法
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+
+        // 读取对象流
+//        try (FileInputStream fis = new FileInputStream(fileName);
+//             ObjectInputStream ois = new ObjectInputStream(fis)) {
+//            ois.readObject();
+//        }
+
+    }
+
+
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        testReadFile();
+        testReadFile2();
+        testReadFlie3();
+        System.out.println("|||||||||||");
+//        testReadFile4();
+        testReadFile5();
+        System.out.println("|||||||||||");
+        testReadFIle6();
+    }
+}
---
+++ summary/src/main/java/summary/io/io/Write.java
@@ -0,0 +1,168 @@
+package summary.io.io;
+
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+/**
+ * program: myStudy
+ * description: Java 创建文件并写入数据的方法总结
+ *
+ * @author: alien
+ * @since: 2020/08/29 19:53
+ */
+public class Write {
+
+    /**
+     * method name: testCreateFile <br/>
+     * description: jdk 1.7 后新增的方式
+
+     * @return: void
+     * @since: 2020/8/29
+     */
+    static void testCreateFile() throws IOException {
+        String fileName = "./newFile1.txt";
+
+        Path path = Paths.get(fileName);
+
+        try (BufferedWriter bufferedWriter =
+                     Files.newBufferedWriter(path, StandardCharsets.UTF_8)) {
+            bufferedWriter.write("Hello World 创建文件\n");
+            // 这里可以不需要 flush 方法
+//            bufferedWriter.flush();
+        }
+
+        // 追加写模式
+        try (BufferedWriter writer =
+                     Files.newBufferedWriter(path,
+                             StandardCharsets.UTF_8, StandardOpenOption.APPEND)) {
+            writer.write("Hello World 第二行");
+        }
+    }
+
+    /**
+     * method name: testCresateFile2 <br/>
+     * description: jdk 1.7 以后新增的方式
+
+     * @return: void
+     * @since: 2020/8/29
+     */
+    static void testCreateFile2() throws IOException {
+        String fileName = "./newFile2.txt";
+        Path path = Paths.get(fileName);
+        Files.write(path, "Hello World 创建文件\n".getBytes(StandardCharsets.UTF_8));
+
+        // 追加写模式
+        Files.write(path,
+                "Hello World 第二行".getBytes(StandardCharsets.UTF_8),
+                StandardOpenOption.APPEND);
+    }
+
+    /**
+     * method name: testCreateFile3 <br/>
+     * description: jdk 1.5 引入的方法
+
+     * @return: void
+     * @since: 2020/8/29
+     */
+    static void testCreateFile3() throws IOException {
+        String fileName = "./newFile3.txt";
+
+        try (PrintWriter writer = new PrintWriter(fileName, "UTF-8")) {
+            writer.println("Hello World 创建文件"); // 换行写入
+            writer.println("Hello World 第二行");
+        }
+
+//        // jdk 10 以后引入的重载方法
+//        try (PrintWriter writer = new PrintWriter(fileName, StandardCharsets.UTF_8)) {
+//            writer.println("Hello World 创建文件");
+//            writer.println("Hello World 第二行");
+//        }
+    }
+
+    /**
+     * method name: testCreateFIle4 <br/>
+     * description: 最原始的创建文件并写入的方式
+
+     * @return: void
+     * @since: 2020/8/29
+     */
+    static void testCreateFIle4() throws IOException {
+        String fileName = "./newFile4.txt";
+        final File file = new File(fileName);
+        if (file.createNewFile()) {
+            System.out.println("创建文件成功");
+        } else {
+            System.out.println("文件已存在，不在创建");
+        }
+
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.write("Hello world 创建文件\n");
+        }
+
+        // 追加模式
+        try (FileWriter writer = new FileWriter(file, true)) {
+            writer.write("Hello World 第二行");
+        }
+    }
+
+    /**
+     * method name: testCreateFile5 <br/>
+     * description: 管道流写入文件（最原始的方式之一）
+
+     * @return: void
+     * @since: 2020/8/29
+     */
+    static void testCreateFile5() throws IOException {
+        String fileName = "./newFile5.txt";
+        try (FileOutputStream fos = new FileOutputStream(fileName);
+             OutputStreamWriter osw = new OutputStreamWriter(fos);
+             BufferedWriter writer = new BufferedWriter(osw)) {
+            writer.write("Hello world 创建文件");
+            writer.flush();
+        }
+        // 加缓冲  BufferedWriter
+        // 写入二进制数据 ByteArrayOutputStream
+        // 写入 Java 对象 ObjectOutputStream
+        // ... ... 组合使用，功能灵活
+
+//        try (FileOutputStream fos = new FileOutputStream(fileName);
+//             ObjectOutputStream writer = new ObjectOutputStream(fos)) {
+//            writer.writeObject("Hello world 创建文件");
+//            writer.flush();
+//        }
+
+//        try (FileOutputStream fos = new FileOutputStream(fileName);
+//             ByteArrayOutputStream writer = new ByteArrayOutputStream()) {
+//            writer.write("Hello world 创建文件".getBytes(StandardCharsets.UTF_8));
+//            writer.writeTo(fos);
+//            writer.flush();
+//        }
+    }
+
+    /**
+     * method name: main <br/>
+     * description: 入口函数
+     * @param args:
+     * @return: void
+     * @since: 2020/8/29
+     */
+    public static void main(String[] args) throws IOException {
+        testCreateFile();
+        testCreateFile2();
+        testCreateFile3();
+        testCreateFIle4();
+        testCreateFile5();
+    }
+}
